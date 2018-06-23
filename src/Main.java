b8beb2b7e46230dbab7beeb0b9cd06a159a87499
harihaja.ampilahy <harihaja.ampilahy@banregio.com>
2018-06-23 11:10:11 -0500

    a

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,13 @@
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-
-
+import ca.springmaven.fr.Home;
 
 public class Main {
-	
-	
-	public static void main (String[] args){
-		
-		AbstractApplicationContext  appCtxt = new ClassPathXmlApplicationContext();
-		
-		
-		
-		
-		
-		
-		
-		System.out.println("Styst   ");
-		
-		
-		
-		
-		
-		
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		AbstractApplicationContext appCtxt = new ClassPathXmlApplicationContext("springMaven.xml");
+		Home home = (Home) appCtxt.getBean("home");
+		System.out.println("City " + home.getCity());
+		System.out.println("Number " + home.getNumber());
 	}
-
-
-	
 }
---
+++ src/ca/springmaven/fr/Home.java
@@ -0,0 +1,25 @@
+package ca.springmaven.fr;
+
+public class Home {
+	private Integer number;
+	private String city;
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+
+	public void setNumber(Integer number) {
+		this.number = number;
+	}
+
+	public Home() {
+	}
+}
