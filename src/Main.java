88e349a293308a067f8be9d2ff44dad7eb832d89
harihaja.ampilahy <harihaja.ampilahy@banregio.com>
2018-06-23 10:10:36 -0500

    push

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+
+
+public class Main {
+	
+	
+	public static void main (String[] args){
+		
+		AbstractApplicationContext  appCtxt = new ClassPathXmlApplicationContext();
+		
+		
+		
+		
+		
+		
+		
+		System.out.println("Styst   ");
+		
+		
+		
+		
+		
+		
+	}
+
+
+	
+}
