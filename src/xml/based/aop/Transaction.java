acbaa244185d14b2e0439f1694b8f8004db1b617
harihaja.ampilahy <harihaja.ampilahy@banregio.com>
2018-07-03 12:50:05 -0500

    mail

---
+++ src/xml/based/aop/Transaction.java
@@ -0,0 +1,29 @@
+package xml.based.aop;
+
+public class Transaction {
+
+	private int id;
+
+	private String description;
+
+	public int getId() {
+		return id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Transaction() {
+
+	}
+
+}
