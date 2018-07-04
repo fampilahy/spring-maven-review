0c43383ee910a824a17f1b8c6ba3bca67896b260
harihaja.ampilahy <harihaja.ampilahy@banregio.com>
2018-07-04 18:37:41 -0500

    mail

--- src/xml/based/aop/Printer.java
+++ src/xml/based/aop/Printer.java
@@ -7,8 +7,12 @@ public class Printer {
 	}
 	
 	
-	public static void print(){
-		System.out.println("We are inside printer");
-	}
+//	public static void print(){
+////		System.out.println("We are inside printer");
+//	}
 
+	public static void print(Object obj){
+		System.out.println("We are inside printer ==> "+obj.getClass());
+	}
+	
 }
